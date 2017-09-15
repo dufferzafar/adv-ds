be08761c077bba6bb0c79ee7c7258a51760d0672
Shadab Zafar <dufferzafar0@gmail.com>
2017-09-15 23:21:07 +0530

    New Deterministic Selection Implementation
    Algorithm from: https://www.ics.uci.edu/~eppstein/161/960130.html

--- select-deterministic.py
+++ select-deterministic.py
@@ -1,90 +1,51 @@
 import sys
 
 
-def swap(arr, i, j):
-    """Inplace swap values in an array."""
-    arr[i], arr[j] = arr[j], arr[i]
-
-
-def partition(arr, beg, end, pvt_idx):
-    """
-    Inplace partition-ing of arr[beg:end] around pivot (arr[pvt_idx]).
-
-    Return the updated index of pivot.
-    """
-
-    pvt = arr[pvt_idx]
-
-    # Move pivot to the end
-    swap(arr, end, pvt_idx)
-
-    # This will store the new index of pivot
-    pvt_new_idx = beg
-
-    for idx in range(beg, end):
-
-        # Move values smaller than pivot to the beginning of array
-        if arr[idx] < pvt:
-            swap(arr, idx, pvt_new_idx)
-            pvt_new_idx += 1
-
-    # Move pivot to new position
-    swap(arr, end, pvt_new_idx)
-
-    return pvt_new_idx
-
-
 def median_of_5(arr):
     """Return median of a group of atmost 5 integers."""
     return sorted(arr)[len(arr)//2]
 
 
-def median_of_medians(arr, beg, end):
+def select(arr, k):
     """
-    Find an approx median of arr[beg:end].
+    Return the k-th smallest integer in arr.
 
-    This is then used as a pivot selection strategy in deterministic select
-
-    This is NOT inplace as it creates a new list to store medians of subgroups
-
-    CLRS/Wikipedia have an algorithm that doesn't create a new list but are
-    a tad bit confusing.
-
-    https://en.wikipedia.org/wiki/Median_of_medians
+    Reference: https://www.ics.uci.edu/~eppstein/161/960130.html
     """
-    # For less than 5 integers, just return median
-    if end - beg < 5:
-        return median_of_5(arr)
 
-    # Find medians of 5-integer-subgroups
-    medians = []
-    for grp_beg in range(beg, end, 5):
-        grp_end = min(grp_beg+5, end+1)
-        medians.append(median_of_5(arr[grp_beg:grp_end]))
+    # For less than 5 numbers, just return answer
+    if len(arr) < 5:
+        return sorted(arr)[k]
+
+    # Find medians of 5-number-subgroups
+    medians = [
+        median_of_5(arr[i:i+5])
+        for i in range(0, len(arr), 5)
+    ]
 
     # Compute median of n/5 medians-of-5
-    return select(medians, 0, len(medians)-1, len(medians)//2)
+    median_of_median = select(medians, (len(medians)-1)//2)
 
+    # Partition arr into 3 parts arround median_of_median
+    lesser, higher, equal = [], [], []
 
-def select(arr, beg, end, k):
-    """Return the k-th smallest integer in arr[beg:end]."""
+    for num in arr:
+        if num < median_of_median:
+            lesser.append(num)
+        elif num > median_of_median:
+            higher.append(num)
+        else:
+            equal.append(num)
 
-    # If the list has only 1 integer
-    if beg == end:
-        return arr[beg]
+    # Decide which array to recur on
+    if k < len(lesser):
+        return select(lesser, k)
 
-    # Selection with median-of-medians pivot selection
-    pvt = median_of_medians(arr, beg, end)
-    # NOTE: This assumes that arr has unique integers
-    pvt_idx = arr.index(pvt)
-    pvt_new_idx = partition(arr, beg, end, pvt_idx)
+    elif k < len(lesser) + len(equal):
+        return equal[0]
 
-    if k == pvt_new_idx:
-        return arr[pvt_new_idx]
-    elif k < pvt_new_idx:
-        return select(arr, beg, pvt_new_idx - 1, k)
-    elif k > pvt_new_idx:
-        return select(arr, pvt_new_idx + 1, end, k)
+    else:
+        return select(higher, k-len(lesser)-len(equal))
 
 
 def read_int_file_to_list(filename):
@@ -111,6 +72,6 @@ if __name__ == '__main__':
 
     # NOTE: This assumes 1 based order statistics
     # so minimum is the first order statistic
-    ans = select(arr, 0, len(arr)-1, k-1)
+    ans = select(arr, k-1)
 
     print("Integer with rank %d is: %d" % (k, ans))
